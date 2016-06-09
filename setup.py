5fb46e5faf47b29bf2b2fecea288d441d5cd97ba
= <keyboardmailtesting@gmail.com>
2016-06-09 17:30:00 -0400

    Pushing stuff to GitHub before PyPi test release

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name="json_rpc",
+    packages=["json_rpc"],
+    version="0.1",
+    description="A Python 3 module that provides packet generation and "
+                "validation for the JSON-RPC 2.0 specification",
+    author="RPiAwesomeness",
+    author_email="keyboadmailtesting@gmail.com",
+    url="https://github.com/rpiawesomeness/JSON-RPC",
+    download_url="https://github.com/peterldowns/mypackage/tarball/0.1",
+    keywords=["json", "rpc", "packet", "data"],
+    classifiers=[],
+)
