8ccf7b6904eded67110a60bdb5105f7e712ca4b6
RPiAwesomeness <keyboardmailtesting@gmail.com>
2016-06-21 18:21:17 -0400

    Updating tags!

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name="jrpc_helper",
     packages=["jrpc_helper"],
-    version="0.1",
+    version="0.2",
     description="A Python 3 module that provides packet generation and "
                 "validation for the JSON-RPC 2.0 specification",
     author="RPiAwesomeness",
     author_email="keyboadmailtesting@gmail.com",
     url="https://github.com/CactusBot/py_jrpc",
-    download_url="https://github.com/CactusBot/py_jrpc/tarball/0.1",
+    download_url="https://github.com/CactusBot/py_jrpc/tarball/0.2",
     keywords=["json", "rpc", "packet", "data"],
     classifiers=[],
 )
